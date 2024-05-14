3dd3b9a801f5bb4288d717ed2bd701aac0af7629
KatrinaZuus <katrinazuus@gmail.com>
2024-05-14 18:39:58 +0400

    start colab

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Header from "./components/Header";
+import Invoices from './pages/Invoices';
+import CreateInvoice from "./pages/CreateInvoice"
+
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Invoices />} />
+        <Route path="/createinvoice" element={<CreateInvoice />} />
+        {/* <Route path="/Editinvoice" element={<EditInvoice />} />
+        <Route path="/Invoices/:id" element={<ViewInvoice />} /> */}
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/CreateInvoice.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+
+export default function CreateInvoice() {
+    return (
+        <div>
+           <p>hhhh</p>
+        </div>
+    )
+}
+
+// const MainContainer = styled.div`
+//      width: 100%;
+//     padding: 33px 24px 24px 22px;
+// `
+
