af7cbd7847e0bc4503791eb6efe80a905d8a3cf6
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-05-15 00:52:51 +0400

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Invoices from './pages/Invoices';
-import CreateInvoice from "./pages/CreateInvoice"
-
+import Invoices from "./pages/Invoices";
+import CreateInvoice from "./pages/CreateInvoice";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<Invoices />} />
-        <Route path="/createinvoice" element={<CreateInvoice />} />
-        {/* <Route path="/Editinvoice" element={<EditInvoice />} />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Invoices />} />
+          <Route path="/createinvoice" element={<CreateInvoice />} />
+          {/* <Route path="/Editinvoice" element={<EditInvoice />} />
         <Route path="/Invoices/:id" element={<ViewInvoice />} /> */}
-      </Routes>
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -1,7 +1,114 @@
 import React from "react";
+import styled from "styled-components";
+import arrowDown from "/images/icon-arrow-down.svg";
+import plus from "/images/icon-plus.svg";
 
 export default function Invoices() {
-    return (
-        <div>invoice</div>
-    )
+  async function foo() {
+    const res = await fetch("http://localhost:3000/people");
+    const data = await res.json();
+  }
+  return (
+    <CommonInvoicesDiv>
+      <InvoicecCounterCon>
+        <InvoiceCountersDiv>
+          <span>Invoices</span>
+          <p>7 invoices</p>
+        </InvoiceCountersDiv>
+        <FilterAndNew>
+          <FilterCon>
+            <span>Filter</span>
+            <img src={arrowDown} alt="arrow down" />
+          </FilterCon>
+          <BtnCon>
+            <Btn>New</Btn>
+            <Circle></Circle>
+          </BtnCon>
+        </FilterAndNew>
+      </InvoicecCounterCon>
+    </CommonInvoicesDiv>
+  );
 }
+
+const BtnCon = styled.div`
+  position: relative;
+`;
+const Circle = styled.div`
+  width: 3.2rem;
+  height: 3.2rem;
+  border-radius: 50%;
+  background: #fff;
+  position: absolute;
+  top: 0.6rem;
+  left: 0.6rem;
+  z-index: 0;
+`;
+const Btn = styled.button`
+  text-align: right;
+  color: #fff;
+  font-family: "League Spartan";
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 15px;
+  letter-spacing: -0.25px;
+  border-radius: 24px;
+  background: black /*var(--01, #7c5dfa) */;
+  border: none;
+  padding: 1.5rem;
+  width: 9rem;
+  background-image: url(${plus});
+  background-repeat: no-repeat;
+  background-position: right;
+`;
+const InvoicecCounterCon = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const FilterCon = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+  & > span {
+    color: var(--08, #0c0e16);
+    text-align: right;
+    font-family: "League Spartan";
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 15px; /* 100% */
+    letter-spacing: -0.25px;
+  }
+`;
+const FilterAndNew = styled.div`
+  display: flex;
+  gap: 1.8rem;
+  align-items: center;
+`;
+const InvoiceCountersDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  & > span {
+    color: var(--08, #0c0e16);
+    font-family: "League Spartan";
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -0.75px;
+  }
+  & > p {
+    color: var(--06, #888eb0);
+    font-family: "League Spartan";
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 15px;
+    letter-spacing: -0.1px;
+  }
+`;
+const CommonInvoicesDiv = styled.div`
+  background: var(--11, #f8f8fb);
+  padding: 3.6rem 2.4rem 10.5rem 2.4rem;
+`;
